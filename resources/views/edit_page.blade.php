a2e4ad254ff4582b2cb45b3535a083e53f89c2a5
faiz2022 <student_is_the_best_2006@yahoo.com>
2022-05-18 17:36:33 +0500

    some styling update in backend

--- resources/views/edit_page.blade.php
+++ resources/views/edit_page.blade.php
@@ -4,11 +4,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <style>
-      form .form-control{
-        border:1px solid #000 !important;    
-      }
-      </style>
+  
 
 @section('content')
  <!-- BEGIN: Content-->
